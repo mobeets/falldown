ea80053ace257d1a1acb30b8393b389525d11228
Mani Puram <manikpuram1@gmail.com>
2026-06-23 12:37:46 -0500

    current RNN updates

--- analysis/behavior.py
+++ analysis/behavior.py
@@ -350,16 +350,16 @@ def plot_rt_vs_distance(choices, step=1):
 
 #%% load data
 
-fnm = '../logs/unknown-2026-01-30T20-34-28-374Z-jr71.json'
-fnm = '../logs/unknown-2026-02-10T20-42-37-419Z-99s9.json'
-fnm = '../analysis/2-11-trial1.json'
-fnm = '../logs/unknown-2026-02-12T19-44-31-094Z-suyb.json'
-fnm = '../logs/unknown-2026-02-12T19-51-47-046Z-10wy.json'
+#fnm = '../logs/unknown-2026-01-30T20-34-28-374Z-jr71.json'
+#fnm = '../logs/unknown-2026-02-10T20-42-37-419Z-99s9.json'
+#fnm = '../analysis/2-11-trial1.json'
+#fnm = '../logs/unknown-2026-02-12T19-44-31-094Z-suyb.json'
+#fnm = '../logs/unknown-2026-02-12T19-51-47-046Z-10wy.json'
 # fnm = '../logs/unknown-2026-02-12T20-45-28-597Z-ash0.json'
-fnm = '../logs//RAH-2026-02-15T15-42-43-790Z-oopr.json'
-fnm = '../logs/EMU/YFV-2026-02-20T20-13-23-929Z-iel9.json'
-fnm = 'real_trial1.json'
-fnm = '../logs/EMU/YFW-2026-04-29T17-01-30-883Z-ikoe.json'
+#fnm = '../logs//RAH-2026-02-15T15-42-43-790Z-oopr.json'
+#fnm = '../logs/EMU/YFV-2026-02-20T20-13-23-929Z-iel9.json'
+#fnm = 'real_trial1.json'
+fnm = '../analysis/cloud study data/65D6694BE06947289BE4336BC1DE271A-019e9464-b9d3-798d-aa65-c87d82961db6-019e8386-74e7-7359-827b-6b4e4bc47db9-2026-06-04T21-03-48-346Z-fg8d.json'
 data = load(fnm)
 
 #%% visualize task
